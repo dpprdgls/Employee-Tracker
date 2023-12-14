16b57c3e87cb9400dbed19c9f1d33fd9b86538a7
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2023-12-14 12:59:16 -0800

    define table relations

---
+++ models/index.js
@@ -0,0 +1,40 @@
+const Department = require('./department');
+const Employee = require('./employee');
+const Role = require('./role');
+
+//Lets describe the relations between the tables
+
+//Role belongs to Department
+
+Role.belongsTo(Department, {
+    foreignKey: 'department_id',
+    onDelete: 'CASCADE',
+});
+
+//Department has many Roles
+
+Department.hasMany(Role, {
+    foreignKey: 'department_id',
+    onDelete: 'CASCADE',
+});
+
+//Employee belongs to Role
+
+Employee.belongsTo(Role, {
+    foreignKey: 'role_id',
+    onDelete: 'CASCADE',
+});
+
+// Role has one employee 
+
+Role.hasOne(Employee, {
+    foreignKey: 'role_id',
+    onDelete: 'CASCADE',
+});
+
+//Employee has one Manager (Employee)
+
+Employee.hasOne(Employee, {
+    foreignKey:'manager_id',
+    onDelete: 'CASCADE',
+});
